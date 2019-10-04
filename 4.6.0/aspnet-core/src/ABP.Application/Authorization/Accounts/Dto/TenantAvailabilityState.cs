485f478f8b3d446d44a31d4a167a72ec4ce8d85a
JJZhang <nbnbnb.zhang@gmail.com>
2019-10-04 20:52:49 +0800

    Clean

--- 4.6.0/aspnet-core/src/ABP.Application/Authorization/Accounts/Dto/TenantAvailabilityState.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ABP.Authorization.Accounts.Dto
-{
-    public enum TenantAvailabilityState
-    {
-        Available = 1,
-        InActive,
-        NotFound
-    }
-}
--- 4.6.0/aspnet-core/src/ABP.Web.Core/Authentication/External/IExternalAuthConfiguration.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace ABP.Authentication.External
-{
-    public interface IExternalAuthConfiguration
-    {
-        List<ExternalLoginProviderInfo> Providers { get; }
-    }
-}
