a1f4b9731be54fb8db0533d6919df7c7b0ee39e7
JJZhang <nbnbnb.zhang@gmail.com>
2019-10-04 20:55:21 +0800

    4.8.0

---
+++ 4.8.0/src/Acme.SimpleTaskApp.EntityFrameworkCore/EntityFrameworkCore/SimpleTaskAppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Abp.Zero.EntityFrameworkCore;
+using Acme.SimpleTaskApp.Authorization.Roles;
+using Acme.SimpleTaskApp.Authorization.Users;
+using Acme.SimpleTaskApp.MultiTenancy;
+
+namespace Acme.SimpleTaskApp.EntityFrameworkCore
+{
+    public class SimpleTaskAppDbContext : AbpZeroDbContext<Tenant, Role, User, SimpleTaskAppDbContext>
+    {
+        /* Define a DbSet for each entity of the application */
+        
+        public SimpleTaskAppDbContext(DbContextOptions<SimpleTaskAppDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
